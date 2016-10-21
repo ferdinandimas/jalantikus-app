711143a318d16dc777a2460caa1564b68d9d798f
Ferdinand Dimas <bendot.tue@gmail.com>
2016-10-21 15:24:12 +0700

    Create multiple images timeline

--- app/models/homeModel.js
+++ app/models/homeModel.js
@@ -12,18 +12,32 @@ define(
                 var that = this;
 
                 $.ajax({
-                    url: _config.jtAPI + "getArticles/limit/10/page/" + _page + "/order/published/detail/id,title,slug,image,user,published",
+                    url: _config.jtAPI + "live/getArticles/limit/9/page/" + _page + "/order/published/detail/id,title,slug,image,user,published,description_images",
                     dataType: "json",
                     async: false,
                     success: function (result) {
                         ajaxResult = result.response;
 
+                        var _buff = 0;
                         _.each(ajaxResult, function (value) {
+                            _buff++;
+
+                            console.log(value.title, value.description_images.length, _buff);
+                            if (_buff >= 3) {
+                                _buff = 0;
+
+                                if (value.description_images.length >= 3) {
+                                    value.multiple_images = true;
+                                }
+                            }
+
+                            console.log(value);
+
                             that.add(value);
                         });
                     },
                     error: function(XMLHttpRequest, textStatus, errorThrown) {
-                        jt.log("Ajax home error", errorThrown, _config.jtAPI + "getArticles/limit/10/page/" + _page + "/order/published/detail/id,title,slug,image,user,published");
+                        jt.log("Ajax home error", errorThrown, _config.jtAPI + "live/getArticles/limit/10/page/" + _page + "/order/published/detail/id,title,slug,image,user,published,description_images");
                     }
                 });
             },
