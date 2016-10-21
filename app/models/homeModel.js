dca7686cc41985d702e0c62e2606c6df2f16cbb1
christopher.joshua <christopher.joshua@mainspring.technology>
2016-10-21 15:19:51 +0700

    Perbaikan detail, loader

--- app/models/homeModel.js
+++ app/models/homeModel.js
@@ -22,8 +22,12 @@ define(
                             that.add(value);
                         });
                     },
+                    timeout: 10000,
                     error: function(XMLHttpRequest, textStatus, errorThrown) {
-                        jt.log("Ajax home error", errorThrown, _config.jtAPI + "getArticles/limit/10/page/" + _page + "/order/published/detail/id,title,slug,image,user,published");
+                        console.log("Ajax home error", errorThrown, _config.jtAPI + "getArticles/limit/10/page/" + _page + "/order/published/detail/id,title,slug,image,user,published");
+                        
+                        $(".app-load").css("display", "none");
+                        $(".app-retry").css("display", "block");
                     }
                 });
             },
