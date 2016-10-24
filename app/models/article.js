b3fa9f0cf87dd750fedb8df2fe34ac903a553425
Ferdinand Dimas <bendot.tue@gmail.com>
2016-10-24 13:15:04 +0700

    Improve article detail

--- app/models/article.js
+++ app/models/article.js
@@ -8,7 +8,7 @@ define(
                 this.slug = (typeof options != "undefined" && typeof options.slug != "undefined" ? options.slug : 1);
             },
             url       : function () {
-                return _config.jtAPI + "getArticle/" + this.slug;
+                return _config.jtAPI + (_config.environment == "dev" ? "live/" : null) + "getArticle/" + this.slug + "/detail/title,image,user,published,description";
             },
             parse     : function (result) {
                 return result.response;
