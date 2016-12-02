8929f7eab66fbdcc98335fc27e5b133fa5aafdf4
christopher.joshua <christopher.joshua@mainspring.technology>
2016-12-02 14:19:48 +0700

    Header berbeda untuk beranda

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -93,6 +93,15 @@ define(
                 $("a.usermenu-item").removeClass("active").each(function () {
                     if ($(this).attr("href") == "#" + Backbone.history.getFragment()) {
                         // $("#app-toolbar .header-description").html($(this).find(".usermenu-item-detail").html());
+                        if($(this).find(".usermenu-item-detail").html().trim() != "Beranda")
+                        {
+                            $(".app-header .header-description").html($(this).find(".usermenu-item-detail").html());
+                            $(".app-logo").hide();
+                        }
+                        else
+                        {
+                            $(".app-logo").show();
+                        }
                         $(this).addClass("active");
                     }
                 });
@@ -281,7 +290,7 @@ define(
                 $(".app-content-container").on("scroll touchmove",  function(e)
                 {
                     _pos = $(".app-content-container");
-                    if(_pos.scrollTop() > 0)
+                    if(_pos.scrollTop() > 0 && Backbone.history.getFragment() == "")
                     {
                         $(".app-toolbar").removeClass("on-top");
                     }
