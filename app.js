6a08b514a03c98f1a0ec84099c717aaa016dd832
Ferdinand Dimas <bendot.tue@gmail.com>
2016-12-07 14:37:21 +0700

    Bug Fixing

--- app.js
+++ app.js
@@ -67,6 +67,17 @@ require(
 				$(".splash").fadeOut("fast");
 			}
 
+			window.addEventListener("error", handleError, true);
+
+			function handleError(evt) {
+				if (evt.message) { // Chrome sometimes provides this
+					alert("error: " + evt.message + " at linenumber: " + evt.lineno + " of file: " + evt.filename);
+				}
+				else {
+					alert("error: " + evt.type + " from element: " + (evt.srcElement || evt.target));
+				}
+			}
+
 			if ($("#app-body .app-refreshed").length == 0) {
 				$("#app-body").append(
 					'<div class="app-refreshed"></div>'
