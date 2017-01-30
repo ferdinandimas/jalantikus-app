8eb85b7972e972e6faf2e72ec7588cd35e840ea3
Ferdinand Dimas <bendot.tue@gmail.com>
2017-01-30 10:41:54 +0700

    Autoupdate alert

--- app.js
+++ app.js
@@ -815,8 +815,23 @@ require(
 				}
 			});
 
-			cordova.getAppVersion.getVersionNumber(function (version) {
-				alert(version);
+			$.getJSON(_config.jtAPI + "live/jtApps", function (data) {
+				if (typeof data.response != "undefined" && typeof data.response.version != "undefined") {
+					cordova.getAppVersion.getVersionNumber(function (version) {
+						if (version != data.response.version) {
+							navigator.notification.confirm(
+								"Versi Baru Telah Tersedia!",
+								function (confirmation) {
+									if (confirmation == 2) {
+										cordova.plugins.market.open('com.jalantikus.app');
+									}
+								},
+								"Keluar",
+								[ "Nanti Saja", "Update Sekarang" ]
+							);
+						}
+					});
+				}
 			});
 		});
 	}
